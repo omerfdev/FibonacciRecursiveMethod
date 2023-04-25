d5ca37ce653f0eeb00209ad25d611c97cec5ee3f
omeralmali1 <omeralmali1@gmail.com>
2023-04-26 01:57:54 +0300

    Add project files.

---
+++ FibonacciRecursiveMethod/Program.cs
@@ -0,0 +1,9 @@
+﻿int FactorialMethod(int number)
+{
+    if (number == 1 || number == 0) return 1;
+    return number * FactorialMethod(number - 1);
+    
+}
+
+Console.WriteLine(FactorialMethod(5));
+Console.ReadLine();
