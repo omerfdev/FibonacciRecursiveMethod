b9854882ef1a370230100e6340f78ac2cb6e43b7
omerfdev <omerfdev@gmail.com>
2023-06-18 21:35:26 +0300

    Commit

--- FibonacciRecursiveMethod/Program.cs
+++ FibonacciRecursiveMethod/Program.cs
@@ -1,1 +1,23 @@
 ﻿
+
+namespace FibonacciRecursive
+{
+    internal class Program
+    {
+        public delegate int FactorialMethodDelegate(int number);
+
+        public static int FactorialMethods(int number)
+        {
+            if (number == 1 || number == 0)
+                return 1;
+            return number * FactorialMethods(number - 1);
+        }
+
+        static void Main(string[] args)
+        {
+            FactorialMethodDelegate factorialDelegate = FactorialMethods;
+            Console.WriteLine(factorialDelegate(Convert.ToInt32(Console.ReadLine())));
+        }
+    }
+}
+
