e2f7f0cba155ff62af0a9e65c458f63931fde7e7
omerfdev <omerfdev@gmail.com>
2023-06-18 21:33:35 +0300

    Commit

--- FibonacciRecursiveMethod/Program.cs
+++ FibonacciRecursiveMethod/Program.cs
@@ -1,8 +1,1 @@
-﻿int FactorialMethod(int number)
-{
-    if (number == 1 || number == 0) return 1;
-    return number * FactorialMethod(number - 1);
-    
-}
-
-Console.WriteLine(FactorialMethod(Convert.ToInt32(Console.ReadLine())));
+﻿
